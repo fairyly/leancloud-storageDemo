0115a05bb515ba925b2e2c5fdf75c86a90c4b576
fairyly <fairyilys@gmail.com>
2018-02-04 15:23:13 +0800

    add leancloud storage demo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# leancloud-storageDemo
-leancloud-storageDemo
+# LeanCloud 存储 - JavaScript 入门 Demo
+
+入口位于 login 文件夹下的 login.html
---
+++ initLeanCloud.js
@@ -0,0 +1,8 @@
+// LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
+// https://leancloud.cn/docs/sdk_setup-js.html#初始化
+var APP_ID = 'dM7VDvl6pYCGVsrhpjFUPTzv-gzGzoHsz';
+var APP_KEY = '1tWRv0vgoDPpJ2oDm0rKJSOP';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
